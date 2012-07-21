5bfb8c0f06197e697ac4f8822423d9b9acd4e3d6
FDR <fdr@fdr.hu>
2012-07-21 17:22:59 +0200

    Reordered channel page. Made reverse clickable.

---
+++ src/mixer.h
@@ -0,0 +1,127 @@
+#ifndef _MIXER_H_
+#define _MIXER_H_
+
+//MAX = 10000
+//MIN = -10000
+#define CHAN_MULTIPLIER 100
+#define PCT_TO_RANGE(x) ((s16)x * CHAN_MULTIPLIER)
+#define RANGE_TO_PCT(x) ((s16)x / CHAN_MULTIPLIER)
+#define CHAN_MAX_VALUE (100 * CHAN_MULTIPLIER)
+#define CHAN_MIN_VALUE (-100 * CHAN_MULTIPLIER)
+
+enum Mode {
+    MODE_1,
+    MODE_2,
+    MODE_3,
+    MODE_4,
+};
+
+enum CurveType {
+    CURVE_NONE,
+    CURVE_MIN_MAX,
+    CURVE_ZERO_MAX,
+    CURVE_GT_ZERO,
+    CURVE_LT_ZERO,
+    CURVE_ABSVAL,
+    CURVE_EXPO,
+    CURVE_3POINT,
+    CURVE_5POINT,
+    CURVE_7POINT,
+    CURVE_9POINT,
+    CURVE_11POINT,
+    CURVE_13POINT,
+};
+#define CURVE_MAX CURVE_13POINT
+
+
+struct Curve {
+    enum CurveType type;
+    s8 points[MAX_POINTS];
+};
+
+//The followingis defined bythe target
+extern const char *tx_input_str[NUM_TX_INPUTS];
+extern const char *tx_button_str[NUM_TX_BUTTONS];
+
+enum TemplateType {
+    MIXERTEMPLATE_NONE,
+    MIXERTEMPLATE_SIMPLE,
+    MIXERTEMPLATE_EXPO_DR,
+    MIXERTEMPLATE_COMPLEX,
+};
+#define MIXERTEMPLATE_MAX MIXERTEMPLATE_COMPLEX
+
+enum MuxType {
+    MUX_REPLACE,
+    MUX_MULTIPLY,
+    MUX_ADD,
+};
+
+enum SwashType {
+    SWASH_TYPE_NONE,
+    SWASH_TYPE_120,
+    SWASH_TYPE_120X,
+    SWASH_TYPE_140,
+    SWASH_TYPE_90,
+};
+#define MIX_SRC(x) ((x) & 0x7F)
+#define MIX_SRC_IS_INV(x) ((x) & 0x80)
+#define MIX_SET_SRC_INV(x, y) x = (y) ? ((x) | 0x80) : ((x) & ~0x80)
+struct Mixer {
+    u8 src;
+    u8 dest;
+    u8 sw;
+    struct Curve curve;
+    s8 scalar;
+    s8 offset;
+    enum MuxType mux;
+};
+
+enum LimitFlags {
+    CH_REVERSE     = 0x01,
+    CH_FAILSAFE_EN = 0x02,
+};
+
+struct Limit {
+    u8 flags;
+    u8 safetysw;
+    s8 safetyval;
+    s8 max;
+    s8 min;
+    s8 failsafe;
+};
+
+struct Trim {
+    u8 src;
+    u8 pos;
+    u8 neg;
+    u8 step;
+};
+
+/* Curve functions */
+s16 CURVE_Evaluate(s16 value, struct Curve *curve);
+const char *CURVE_GetName(struct Curve *curve);
+u8 CURVE_NumPoints(struct Curve *curve);
+
+/* Mixer functions */
+int MIX_GetMixers(int ch, struct Mixer *mixers, int count);
+int MIX_SetMixers(struct Mixer *mixers, int count);
+
+void MIX_GetLimit(int ch, struct Limit *limit);
+void MIX_SetLimit(int ch, struct Limit *limit);
+
+void MIX_SetTemplate(int ch, enum TemplateType value);
+enum TemplateType MIX_GetTemplate(int ch);
+
+void MIX_InitMixer(struct Mixer *mixer, u8 ch);
+
+void MIX_ApplyMixer(struct Mixer *mixer, s16 *raw);
+void MIX_EvalMixers(s16 *raw);
+u8 MIX_ReadInputs(s16 *raw);
+void MIX_CreateCyclicInputs(s16 *raw);
+struct Mixer *MIX_GetAllMixers();
+
+struct Trim *MIX_GetAllTrims();
+
+s16 MIX_ApplyLimits(u8 channel, struct Limit *limit, s16 *raw);
+#endif
---
+++ src/protocol/flysky_a7105.c
@@ -0,0 +1,110 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "target.h"
+#include "interface.h"
+#include "mixer.h"
+
+#ifdef PROTO_HAS_A7105
+const u8 A7105_regs[] = {
+     -1,  0x42, 0x00, 0x14, 0x00,  -1 ,  -1 , 0x00, 0x00, 0x00, 0x00, 0x01, 0x21, 0x05, 0x00, 0x50,
+    0x9e, 0x4b, 0x00, 0x02, 0x16, 0x2b, 0x12, 0x00, 0x62, 0x80, 0x80, 0x00, 0x0a, 0x32, 0xc3, 0x0f,
+    0x13, 0xc3, 0x00,  -1,  0x00, 0x00, 0x3b, 0x00, 0x17, 0x47, 0x80, 0x03, 0x01, 0x45, 0x18, 0x00,
+    0x01, 0x0f,  -1,
+};
+static u16 packet[11];
+
+void FLYSKY_Initialize() {
+    int i;
+    u8 if_calibration1;
+    u8 vco_calibration0;
+    u8 vco_calibration1;
+
+    A7105_Reset();
+    A7105_WriteID(0x5475c52a);
+    for (i = 0; i < 0x33; i++)
+        if((s8)A7105_regs[i] != -1)
+            A7105_WriteReg(i, A7105_regs[i]);
+
+    A7105_Strobe(A7105_STANDBY);
+
+    //IF Filter Bank Calibration
+    A7105_WriteReg(0x02, 1);
+    while(A7105_ReadReg(0x02))
+        ;
+    if_calibration1 = A7105_ReadReg(0x22);
+    if(if_calibration1 & A7105_MASK_FBCF) {
+        //Calibration failed...what do we do?
+    }
+
+    //VCO Current Calibration
+    A7105_WriteReg(0x24, 0x13); //Recomended calibration from A7105 Datasheet
+
+    //VCO Bank Calibration
+    A7105_WriteReg(0x26, 0x3b); //Recomended limits from A7105 Datasheet
+
+    //VCO Bank Calibrate channel 0?
+    //Set Channel
+    A7105_WriteReg(0x0f, 0); //Should we choose a different channel?
+    //VCO Calibration
+    A7105_WriteReg(0x02, 2);
+    while(A7105_ReadReg(0x02))
+        ;
+    vco_calibration0 = A7105_ReadReg(0x25);
+    if (vco_calibration0 & A7105_MASK_VBCF) {
+        //Calibration failed...what do we do?
+    }
+
+    //Calibrate channel 0xa0?
+    //Set Channel
+    A7105_WriteReg(0x0f, 0xa0); //Should we choose a different channel?
+    //VCO Calibration
+    A7105_WriteReg(0x02, 2);
+    while(A7105_ReadReg(0x02))
+        ;
+    vco_calibration1 = A7105_ReadReg(0x25);
+    if (vco_calibration1 & A7105_MASK_VBCF) {
+        //Calibration failed...what do we do?
+    }
+
+    //Reset VCO Band calibration
+    A7105_WriteReg(0x25, 0x08);
+
+    A7105_Strobe(A7105_STANDBY);
+}
+
+void FLYSKY_BuildPacket()
+{
+    int i;
+    //-100% =~ 0x03e8
+    //+100% =~ 0x07ca
+    //Calculate:
+    //Center = 0x5d9
+    //1 %    = 5
+    packet[0] = 0x5aa;
+    packet[1] = 0x1f3;
+    packet[2] = 0x2000;
+    for (i= 0; i < 8; i++) {
+        if (i > NUM_CHANNELS) {
+            packet[i + 3] = 0;
+            continue;
+        }
+        s32 value = (s32)Channels[i] * 0x1f1 / CHAN_MAX_VALUE + 0x5d9;
+        if (value < 0)
+            value = 0;
+        packet[3 + i] = (u16)value;
+    }
+}
+#endif
