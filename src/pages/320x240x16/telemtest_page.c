2acb0de8229d0609c0b1aa97bf02046b6fd5bd92
FDR <fdr@fdr.hu>
2013-01-01 20:29:25 +0100

    Hungarian language updates

---
+++ src/pages/320x240x16/telemtest_page.c
@@ -0,0 +1,82 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include "pages.h"
+#include "telemetry.h"
+#include "gui/gui.h"
+
+#include "../common/_telemtest_page.c"
+
+static void show_page()
+{
+    const u8 row_height = 20;
+    for(long i = 0; i < 4; i++) {
+        GUI_CreateLabelBox(10, 40 + i*row_height, 40, 16, &TELEM_TXT_FONT,
+                           label_cb, NULL, (void *)(TELEM_TEMP1+i));
+        tp.temp[i] = GUI_CreateLabelBox(60,  40 + i*row_height, 40, 16, &TELEM_ERR_FONT,
+                           telem_cb, NULL, (void *)(TELEM_TEMP1+i));
+    }
+    for(long i = 0; i < 3; i++) {
+        GUI_CreateLabelBox(110, 40 + i*row_height, 40, 16, &TELEM_TXT_FONT,
+                           label_cb, NULL, (void *)(TELEM_VOLT1+i));
+        tp.volt[i] = GUI_CreateLabelBox(155,  40 + i*row_height, 40, 16, &TELEM_ERR_FONT,
+                           telem_cb, NULL, (void *)(TELEM_VOLT1+i));
+    }
+    for(long i = 0; i < 2; i++) {
+        GUI_CreateLabelBox(210, 40 + i*row_height, 40, 16, &TELEM_TXT_FONT,
+                           label_cb, NULL, (void *)(TELEM_RPM1+i));
+        tp.rpm[i]  = GUI_CreateLabelBox(255,  40 + i*row_height, 40, 16, &TELEM_ERR_FONT,
+                           telem_cb, NULL, (void *)(TELEM_RPM1+i));
+    }
+    for(long i = 0; i < 5; i++) {
+        GUI_CreateLabelBox(20, 140 + i*row_height, 60, 16, &TELEM_TXT_FONT,
+                           label_cb, NULL, (void *)(TELEM_GPS_LAT+i));
+        tp.gps[i]  = GUI_CreateLabelBox(100,  140 + i*row_height, 200, 16, &TELEM_ERR_FONT,
+                           telem_cb, NULL, (void *)(TELEM_GPS_LAT+i));
+    }
+    tp.telem = Telemetry;
+    tp.telem.time[0] = 0;
+    tp.telem.time[1] = 0;
+    tp.telem.time[2] = 0;
+}
+
+void PAGE_TelemtestInit(int page)
+{
+    (void)page;
+    PAGE_SetModal(0);
+    PAGE_ShowHeader(PAGE_GetName(PAGEID_TELEMMON));
+    if (telem_state_check() == 0) {
+        GUI_CreateLabelBox(20, 80, 280, 100, &NARROW_FONT, NULL, NULL, tp.str);
+        return;
+    }
+    show_page();
+}
+
+void PAGE_TelemtestModal(void(*return_page)(int page), int page)
+{
+    PAGE_SetModal(1);
+    tp.return_page = return_page;
+    tp.return_val = page;
+    PAGE_RemoveAllObjects();
+
+    PAGE_ShowHeader_ExitOnly(PAGE_GetName(PAGEID_TELEMMON), okcancel_cb);
+    if (telem_state_check() == 0) {
+        GUI_CreateLabelBox(20, 80, 280, 100, &NARROW_FONT, NULL, NULL, tp.str);
+        return;
+    }
+
+    show_page();
+}
---
+++ src/pages/320x240x16/tx_configure.c
@@ -0,0 +1,135 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include "pages.h"
+#include "gui/gui.h"
+#include "config/tx.h"
+#include "config/model.h"
+
+#define MAX_BATTERY_ALARM 12000
+#define MIN_BATTERY_ALARM 3300
+#define MIN_BATTERY_ALARM_STEP 50
+
+u8 page_num;
+guiObject_t *firstObj;
+
+void PAGE_ChangeByName(const char *pageName, u8 menuPage)
+{   // dummy method for devo8, only used in devo10
+    (void)pageName;
+    (void)menuPage;
+}
+#include "../common/_tx_configure.c"
+#define MAX_PAGE 2
+static void _show_page();
+
+static u8 scroll_cb(guiObject_t *parent, u8 pos, s8 direction, void *data)
+{
+    (void)pos;
+    (void)parent;
+    (void)data;
+    s8 newpos = (s8)page_num + (direction > 0 ? 1 : -1);
+    if (newpos < 0)
+        newpos = 0;
+    else if (newpos > MAX_PAGE)
+        newpos = MAX_PAGE;
+    if (newpos != page_num) {
+        page_num = newpos;
+        _show_page();
+    }
+    return page_num;
+}
+
+static void _show_page()
+{
+    if (firstObj) {
+        GUI_RemoveHierObjects(firstObj);
+        firstObj = NULL;
+    }
+    u8 space = 22;
+    u8 row = 40;
+    if (page_num == 0) {
+        firstObj = GUI_CreateLabelBox(16, row, 0, 0,
+                   &SECTION_FONT, NULL, NULL, _tr("Generic settings"));
+        row += space;
+        GUI_CreateLabelBox(16, row+6, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Language:"));
+        GUI_CreateButton(112, row, BUTTON_96, langstr_cb, 0x0000, lang_select_cb, NULL);
+        row += space + 8;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Stick mode:"));
+        GUI_CreateTextSelect(112, row, TEXTSELECT_96, 0x0000, NULL, modeselect_cb, NULL);
+        row += space + 8;
+        GUI_CreateLabelBox(16, row+6, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Touch screen:"));
+        GUI_CreateButton(112, row, BUTTON_96, calibratestr_cb, 0x0000, press_cb, (void *)CALIB_TOUCH);
+        GUI_CreateButton(216, row, BUTTON_48, calibratestr_cb, 0x0000, press_cb, (void *)CALIB_TOUCH_TEST);
+        row += space + 8;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Battery alarm:"));
+        GUI_CreateTextSelect(112, row, TEXTSELECT_96, 0x0000, NULL, batalarm_select_cb, NULL);
+        row += space;
+        GUI_CreateLabelBox(16, row+6, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Sticks:"));
+        GUI_CreateButton(112, row, BUTTON_96, calibratestr_cb, 0x0000, press_cb, (void *)CALIB_STICK);
+        // GUI_CreateButton(216, 140, BUTTON_96, calibratestr_cb, 0x0000, press_cb, (void *)CALIB_STICK_TEST);
+        row += space + 8;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Buzz volume:"));
+        GUI_CreateTextSelect(112, row, TEXTSELECT_96, 0x0000, NULL, common_select_cb, (void *)&Transmitter.volume);
+
+        row += space;
+    } else if (page_num == 1) {
+        firstObj = GUI_CreateLabelBox(16, row, 0, 0, &SECTION_FONT, NULL, NULL, _tr("LCD settings"));
+        row += space;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Backlight:"));
+        GUI_CreateTextSelect(112, row, TEXTSELECT_96, 0x0000, NULL, brightness_select_cb, NULL);
+        row += space;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Dimmer time:"));
+        GUI_CreateTextSelect(112, row, TEXTSELECT_96, 0x0000, NULL, auto_dimmer_time_cb, NULL);
+        row += space;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Dimmer target:"));
+        cp->dimmer_target = GUI_CreateTextSelect(112, row, TEXTSELECT_96, 0x0000, NULL, common_select_cb,
+                (void *)&Transmitter.auto_dimmer.backlight_dim_value);
+        row += space + 8;
+        GUI_CreateLabelBox(16, row, 0, 0, &SECTION_FONT, NULL, NULL, _tr("Telemetry settings"));
+        row += space;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Temperature:"));
+        GUI_CreateTextSelect(112, row, TEXTSELECT_96, 0x0000, NULL, units_cb, (void *)1L);
+        row += space;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Length:"));
+        GUI_CreateTextSelect(112, row, TEXTSELECT_96, 0x0000, NULL, units_cb, (void *)0L);
+    } else if (page_num == 2) {
+        firstObj = GUI_CreateLabelBox(16, row, 0, 0, &SECTION_FONT, NULL, NULL, _tr("Timer settings"));
+        row += space;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL,  _tr("Prealert time:"));
+        GUI_CreateTextSelect(130, row, TEXTSELECT_96, 0x0000,NULL, prealert_time_cb, (void *)0L);
+        row += space;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL, _tr("Prealert intvl:"));
+        GUI_CreateTextSelect(130, row, TEXTSELECT_96, 0x0000, NULL, timer_interval_cb,
+                &Transmitter.countdown_timer_settings.prealert_interval);
+        row += space;
+        GUI_CreateLabelBox(16, row, 0, 0, &DEFAULT_FONT, NULL, NULL,_tr("Timeup intvl:"));
+        GUI_CreateTextSelect(130, row, TEXTSELECT_96, 0x0000, NULL, timer_interval_cb,
+                &Transmitter.countdown_timer_settings.timeup_interval);
+    }
+}
+
+void PAGE_TxConfigureInit(int page)
+{
+    (void)page;
+    cp->enable = CALIB_NONE;
+    PAGE_SetModal(0);
+    PAGE_RemoveAllObjects();
+    PAGE_ShowHeader(PAGE_GetName(PAGEID_TXCFG));
+    GUI_CreateScrollbar(304, 32, 208, MAX_PAGE+1, NULL, scroll_cb, NULL);
+    firstObj = NULL;
+    page_num = 0;
+    _show_page();
+}
