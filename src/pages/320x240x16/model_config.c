09affc8d3515a795abd90f6a02d5059a2c9ae3ef
DestructionBlack <bitbucket.20.dcaron@spamgourmet.com>
2013-02-12 21:45:17 +0100

    Add FR Manuals (Devo10 & Devo 6/8)

---
+++ src/pages/320x240x16/model_config.c
@@ -0,0 +1,87 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include "pages.h"
+#include "gui/gui.h"
+#include "config/model.h"
+
+#include "../common/_model_config.c"
+
+#define gui (&gui_objs.u.modelcfg)
+
+static void okcancel_cb(guiObject_t *obj, const void *data)
+{
+    (void)obj;
+    (void)data;
+    PAGE_RemoveAllObjects();
+    PAGE_ModelInit(-1);  // devo8 doesn't care the page value, while it must be -1 for devo10
+}
+
+static void show_titlerow(const char *header)
+{
+    GUI_CreateLabel(&gui->title, 8, 10, NULL, TITLE_FONT, (void *)header);
+    PAGE_CreateOkButton(264, 4, okcancel_cb);
+}
+
+void MODELPAGE_Config()
+{
+    PAGE_SetModal(1);
+    show_titlerow(Model.type == 0 ? _tr("Helicopter") : _tr("Airplane"));
+    if (Model.type == 0) {
+        u8 i = 40;
+        GUI_CreateLabel(&gui->swashlbl, 8, i, NULL, DEFAULT_FONT, _tr("SwashType:"));
+        GUI_CreateTextSelect(&gui->swash, 136, i, TEXTSELECT_96, NULL, swash_val_cb, NULL);
+        i+=24;
+        GUI_CreateLabel(&gui->invlbl[0], 8, i, NULL, DEFAULT_FONT, _tr("ELE Inv:"));
+        GUI_CreateTextSelect(&gui->inv[0], 136, i, TEXTSELECT_96, swashinv_press_cb, swashinv_val_cb, (void *)1);
+        i+=24;
+        GUI_CreateLabel(&gui->invlbl[1], 8, i, NULL, DEFAULT_FONT, _tr("AIL Inv:"));
+        GUI_CreateTextSelect(&gui->inv[1], 136, i, TEXTSELECT_96, swashinv_press_cb, swashinv_val_cb, (void *)2);
+        i+=24;
+        GUI_CreateLabel(&gui->invlbl[2], 8, i, NULL, DEFAULT_FONT, _tr("COL Inv:"));
+        GUI_CreateTextSelect(&gui->inv[2], 136, i, TEXTSELECT_96, swashinv_press_cb, swashinv_val_cb, (void *)4);
+        i+=24;
+        GUI_CreateLabel(&gui->mixlbl[0], 8, i, NULL, DEFAULT_FONT, _tr("ELE Mix:"));
+        GUI_CreateTextSelect(&gui->mix[0], 136, i, TEXTSELECT_96, NULL, swashmix_val_cb, (void *)1);
+        i+=24;
+        GUI_CreateLabel(&gui->mixlbl[1], 8, i, NULL, DEFAULT_FONT, _tr("AIL Mix:"));
+        GUI_CreateTextSelect(&gui->mix[1], 136, i, TEXTSELECT_96, NULL, swashmix_val_cb, (void *)0);
+        i+=24;
+        GUI_CreateLabel(&gui->mixlbl[2], 8, i, NULL, DEFAULT_FONT, _tr("COL Mix:"));
+        GUI_CreateTextSelect(&gui->mix[2], 136, i, TEXTSELECT_96, NULL, swashmix_val_cb, (void *)2);
+    }
+}
+
+void MODELPROTO_Config()
+{
+    PAGE_SetModal(1);
+    show_titlerow(ProtocolNames[Model.protocol]);
+    proto_strs = PROTOCOL_GetOptions();
+    int row = 40;
+    int pos = 0;
+    long idx = 0;
+    while(idx < NUM_PROTO_OPTS) {
+        if(proto_strs[pos] == NULL)
+            break;
+        GUI_CreateLabel(&gui->protolbl[idx], 8, row, NULL, DEFAULT_FONT, _tr(proto_strs[pos]));
+        GUI_CreateTextSelect(&gui->proto[idx], 136, row, TEXTSELECT_96, NULL, proto_opt_cb, (void *)idx);
+        while(proto_strs[++pos])
+            ;
+        pos++;
+        idx++;
+        row += 24;
+    }
+}
---
+++ src/pages/common/_dialogs.c
@@ -0,0 +1,80 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#define DLG_STR_LEN (80 * 5)
+static guiObject_t *dialog = NULL;
+static char dlgstr[DLG_STR_LEN];
+static u8 disable_safety = 0;//by default =0, means enable
+static guiObject_t *current_selected_obj = NULL; // used for devo10 only
+
+/******************/
+/*  Safety Dialog */
+/******************/
+static void safety_ok_cb(u8 state, void * data)
+{
+    (void)state;
+    (void)data;
+    dialog = NULL;
+    if (current_selected_obj != NULL)
+        GUI_SetSelected(current_selected_obj);
+    PROTOCOL_Init(1);
+}
+
+/**********************/
+/* Low battery Dialog */
+/**********************/
+static void lowbatt_ok_cb(u8 state, void * data)
+{
+    (void)state;
+    (void)data;
+    if (current_selected_obj != NULL)
+        GUI_SetSelected(current_selected_obj);
+    dialog = NULL;
+}
+
+
+void PAGE_DisableSafetyDialog(u8 disable)
+{
+    disable_safety = disable;
+}
+
+static void invalid_simplemixer_cb(u8 state, void *guiObj)
+{
+#define SIMPLE_TEMPLATE "heli_std.ini"
+    if (current_selected_obj != NULL)
+        GUI_SetSelected(current_selected_obj);
+    dialog = NULL;
+    if (state == 1 && guiObj != NULL) {
+        memset(Model.mixers, 0, sizeof(struct Mixer) * (NUM_MIXERS)); // // reset all mixers first
+        CONFIG_ReadTemplate(SIMPLE_TEMPLATE); // load template
+        SIMPLEMIXER_Preset();
+        SIMPLEMIXER_SetChannelOrderByProtocol();
+        Model.mixer_mode = MIXER_SIMPLE;
+        GUI_Redraw(guiObj);
+    }
+}
+
+static void reset_permtimer_cb(u8 state, void *guiObj)
+{
+    if (current_selected_obj != NULL)
+	GUI_SetSelected(current_selected_obj);
+    dialog = NULL;
+    if (state == 1 && guiObj != NULL) {
+	Model.permanent_timer = 0;
+	TIMER_Init();
+	GUI_Redraw(guiObj);
+    }
+}
