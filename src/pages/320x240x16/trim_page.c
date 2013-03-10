fc74945dcc4007947b1f42549986dbd5083c257d
PhracturedBlue <rc2012@pblue.org>
2013-03-09 19:24:31 -0800

    Fix for hubsan protocol

---
+++ src/pages/320x240x16/trim_page.c
@@ -0,0 +1,68 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include "pages.h"
+#include "gui/gui.h"
+
+#include "../common/_trim_page.c"
+
+#define gui (&gui_objs.u.trim)
+#define gui_ed (&gui_objs.u.trimedit)
+static void _show_page()
+{
+    if (Model.mixer_mode == MIXER_STANDARD)
+        PAGE_ShowHeader_ExitOnly(PAGE_GetName(PAGEID_TRIM), MODELMENU_Show);
+    else
+        PAGE_ShowHeader(PAGE_GetName(PAGEID_TRIM));
+    GUI_CreateLabelBox(&gui->inplbl, 4, 40, 64, 15, &NARROW_FONT, NULL, NULL, _tr("Input:"));
+    GUI_CreateLabelBox(&gui->neglbl, 74, 40, 64, 15, &NARROW_FONT, NULL, NULL, _tr("Trim -:"));
+    GUI_CreateLabelBox(&gui->poslbl, 138, 40, 64, 15, &NARROW_FONT, NULL, NULL, _tr("Trim +:"));
+    GUI_CreateLabelBox(&gui->steplbl, 202, 40, 108, 15, &NARROW_FONT, NULL, NULL, _tr("Trim Step:"));
+    struct Trim *trim = MIXER_GetAllTrims();
+    for (u8 i = 0; i < NUM_TRIMS; i++) {
+        GUI_CreateButton(&gui->src[i], 4, 24*i + 66, BUTTON_64x16,
+            trimsource_name_cb, 0x0000, _edit_cb, (void *)((long)i));
+        GUI_CreateLabel(&gui->neg[i], 80, 24*i + 68, NULL, DEFAULT_FONT, (void *)INPUT_ButtonName(trim[i].neg));
+        GUI_CreateLabel(&gui->pos[i], 144, 24*i + 68, NULL, DEFAULT_FONT, (void *)INPUT_ButtonName(trim[i].pos));
+        GUI_CreateTextSelect(&gui->step[i], 208, 24*i + 66, TEXTSELECT_96, NULL, set_trimstep_cb, &trim[i].step);
+    }
+}
+
+static void _edit_cb(guiObject_t *obj, const void *data)
+{
+    (void)obj;
+    struct Trim *trim = MIXER_GetAllTrims();
+    PAGE_SetModal(1);
+    tp->index = (long)data;
+    tp->trim = trim[tp->index];
+
+    PAGE_RemoveAllObjects();
+    PAGE_CreateCancelButton(160, 4, okcancel_cb);
+    PAGE_CreateOkButton(264, 4, okcancel_cb);
+
+    //Row 1
+    GUI_CreateLabel(&gui_ed->srclbl, 8, 48, NULL, DEFAULT_FONT, _tr("Input:"));
+    GUI_CreateTextSelect(&gui_ed->src, 104, 48, TEXTSELECT_96, NULL, set_source_cb, &tp->trim.src);
+    //Row 2
+    GUI_CreateLabel(&gui_ed->neglbl, 8, 72, NULL, DEFAULT_FONT, _tr("Trim +:"));
+    GUI_CreateTextSelect(&gui_ed->neg, 104, 72, TEXTSELECT_96, NULL, set_trim_cb, &tp->trim.pos);
+    //Row 3
+    GUI_CreateLabel(&gui_ed->poslbl, 8, 96, NULL, DEFAULT_FONT, _tr("Trim -:"));
+    GUI_CreateTextSelect(&gui_ed->pos, 104, 96, TEXTSELECT_96, NULL, set_trim_cb, &tp->trim.neg);
+    //Row 3
+    GUI_CreateLabel(&gui_ed->steplbl, 8, 120, NULL, DEFAULT_FONT, _tr("Trim Step:"));
+    GUI_CreateTextSelect(&gui_ed->step, 104, 120, TEXTSELECT_96, NULL, set_trimstep_cb, &tp->trim.step);
+}
---
+++ utils/extract_strings.pl
@@ -0,0 +1,178 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Getopt::Long;
+my $update;
+my $lang;
+my $target;
+my $count;
+my @targets = ("320x240x16", "128x64x1");
+my %targetmap = (
+    devo8 => "320x240x16",
+    devo10 => "128x64x1",
+);
+
+GetOptions("update" => \$update, "language=s" => \$lang, "target=s" => \$target, "count" => \$count);
+if($target && (! $targetmap{$target} || ! (grep {$targetmap{$target} eq $_} @targets))) {
+    my @t = keys(%targetmap);
+    print "Target must be one of: @t\n";
+    exit 1;
+}
+my @lines = `find . -name "*.[hc]" | xargs xgettext -o - --omit-header -k --keyword=_tr --keyword=_tr_noop --no-wrap`;
+my @files;
+my $str = "";
+my $idx = 0;
+my %filemap;
+#Read all strings and put into a hash that maps the containing file to the string
+foreach (@lines) {
+    if(/^\s*$/ || /^msgstr/) {
+        if($str) {
+            foreach my $f (@files) {
+                $filemap{$f} = $str;
+            }
+        }
+        @files = ();
+        $str = "";
+        $idx++;
+    }
+    if(/^#:\s+(.*\S)\s*$/) {
+        foreach my $f (split /\s+/, $1) {
+            push @files, "$f:$idx";
+        }
+    }
+    next unless(/^(msgid|")/);
+    if(/^msgid/) {
+        $str = "";
+    }
+    s/^.*?"(.*)"\s*$/$1/;
+    $str .= $_;
+}
+if($str) {
+    foreach my $f (@files) {
+        $filemap{$f} = $str;
+    }
+}
+#Filter out any files that are not used by the specified target
+if($target) {
+    foreach my $f (keys %filemap) {
+        if(grep {$_ ne $targetmap{$target} && $f =~ /$_/} @targets) {
+            delete $filemap{$f};
+        }
+    }
+}
+#build string list
+my@out = values(%filemap);
+#append template names
+foreach (`head -n 1 filesystem/common/template/*.ini`) {
+    chomp;
+    if(/template=(.*?)\s*$/) {
+        push @out, $1;
+    }
+}
+my %uniq = map {$_ => undef} @out;
+if($count) {
+    printf "%d", scalar(keys(%uniq));
+    exit 0;
+}
+
+if(! $update) {
+    foreach (sort keys %uniq) {
+        print ":$_\n";
+    }
+    exit 0;
+}
+
+@files = glob($lang ?
+              "filesystem/common/language/lang*.$lang"
+              : "filesystem/common/language/lang*");
+
+foreach my $file (@files) {
+    my %strings = %uniq;
+    my %targetstr;
+    my %unused;
+    open my $fh, "<", $file;
+    my $name = <$fh>;
+    my(@lines) = <$fh>;
+    for (my $line = 0; $line < $#lines-1; $line++) {
+        $_ = $lines[$line];
+        chomp;
+        if(/^:(.*)/) {               #/^([<>]?):(.*)/)
+            my($eng) = ($1);
+            next if ($line < $#lines-2 && $lines[$line+1] =~ /^:./);
+            my $next = $lines[++$line];
+            chomp $next;
+            #($next) = ($next =~ /^.(.*)/) if ($missing);
+            if (! exists $uniq{$eng}) {
+                $unused{$eng} = 1;
+            }
+            $strings{$eng} = $next;
+        } elsif(/^\|(\S+?):(.*)/) {
+            my($t, $eng) = ($1, $2);
+            my $next = $lines[++$line];
+            chomp $next;
+            if (! exists $uniq{$eng}) {
+                $unused{$eng} = 1;
+            }
+            $targetstr{$t}{$eng} = $next;
+            $strings{$eng} ||= $next;
+        }
+        
+    }
+    if($target) {
+        #if target is specified, we want to return a filtered list of strings
+        my $outf = $file;
+        $outf =~ s/common/$target/;
+        open $fh, ">", $outf;
+        print $fh $name;
+        $targetstr{$target} ||= {};
+        %strings = (%strings, %{$targetstr{$target}});
+        foreach (sort keys %strings) {
+            if(! $unused{$_} && defined($strings{$_})) {
+                print $fh ":$_\n$strings{$_}\n";
+            }
+        }
+        close $fh;
+    } else {
+        open $fh, ">", $file;
+        print $fh $name;
+        foreach my $str (sort (keys(%strings))) {
+            if ($unused{$str}) {
+                if (scalar(grep {defined $targetstr{$_}{$str}} keys(%targetmap)) != scalar(keys(%targetmap))) {
+                    #print generic if not all targetshave a translation
+                    print $fh "<:$str\n<$strings{$str}\n";
+                }
+                foreach my $key (sort keys %targetmap) {
+                    if(defined $targetstr{$key}{$str}) {
+                        print $fh "<|$key:$str\n<$targetstr{$key}{$str}\n";
+                    }
+                }
+            } elsif(! defined($strings{$str})) {
+                print $fh ">:$str\n";
+            } else {
+                if (scalar(grep {defined $targetstr{$_}{$str}} keys(%targetmap)) != scalar(keys(%targetmap))) {
+                    #print generic if not all targetshave a translation
+                    print $fh ":$str\n$strings{$str}\n";
+                }
+                foreach my $key (sort keys %targetmap) {
+                    if(defined $targetstr{$key}{$str}) {
+                        print $fh "|$key:$str\n$targetstr{$key}{$str}\n";
+                    }
+                }
+            }
+        }
+        close $fh;
+    }
+}
+
+# | xargs xgettext -o - --omit-header -k --keyword=_tr --keyword=_tr_noop --no-wrap | grep -v 'msgstr ""' | grep -v "^#" | grep -v "^$"
+
+#: pages/dialogs.c:100
+#msgid ""
+#"Binding is in progress...\n"
+#"Make sure model is on!\n"
+#"\n"
+#"Pressing OK will NOT cancel binding procedure\n"
+#"but will allow full control of Tx."
+#msgstr ""
+
