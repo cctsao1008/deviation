60dafa0d54dec1e8a6caa51e982fda02e4ff13af
FDR <fdr@fdr.hu>
2013-04-27 21:11:05 +0200

    Hungarian language update.

---
+++ src/pages/common/_main_page.c
@@ -0,0 +1,175 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+static struct main_page * const mp = &pagemem.u.main_page;
+#define gui (&gui_objs.u.mainpage)
+const char *show_box_cb(guiObject_t *obj, const void *data);
+const char *voltage_cb(guiObject_t *obj, const void *data);
+static s16 trim_cb(void * data);
+static s16 bar_cb(void * data);
+void press_icon_cb(guiObject_t *obj, s8 press_type, const void *data);
+void press_icon2_cb(guiObject_t *obj, const void *data);
+void press_box_cb(guiObject_t *obj, s8 press_type, const void *data);
+static u8 _action_cb(u32 button, u8 flags, void *data);
+static s32 get_boxval(u8 idx);
+static void _check_voltage();
+
+struct ImageMap TGLICO_GetImage(int idx);
+
+struct LabelDesc *get_box_font(u8 idx, u8 neg)
+{
+    if(neg) {
+        return idx & 0x02 ? &SMALLBOXNEG_FONT : &BIGBOXNEG_FONT;
+    } else {
+        return idx & 0x02 ? &SMALLBOX_FONT : &BIGBOX_FONT;
+    }
+}
+
+s32 get_boxval(u8 idx)
+{
+    if (idx <= NUM_TIMERS)
+        return TIMER_GetValue(idx - 1);
+    if(idx - NUM_TIMERS <= NUM_TELEM)
+        return TELEMETRY_GetValue(idx - NUM_TIMERS);
+    return RANGE_TO_PCT(MIXER_GetChannel(idx - (NUM_TIMERS + NUM_TELEM + 1), APPLY_SAFETY | APPLY_SCALAR));
+}
+
+const char *show_box_cb(guiObject_t *obj, const void *data)
+{
+    (void)obj;
+    u8 idx = (long)data;
+    if (idx <= NUM_TIMERS) {
+        TIMER_SetString(mp->tmpstr, TIMER_GetValue(idx - 1));
+    } else if(idx - NUM_TIMERS <= NUM_TELEM) {
+        TELEMETRY_GetValueStr(mp->tmpstr, idx - NUM_TIMERS);
+    } else {
+        sprintf(mp->tmpstr, "%3d%%", RANGE_TO_PCT(MIXER_GetChannel(idx - (NUM_TIMERS + NUM_TELEM + 1), APPLY_SAFETY | APPLY_SCALAR)));
+    }
+    return mp->tmpstr;
+}
+
+const char *voltage_cb(guiObject_t *obj, const void *data) {
+    (void)obj;
+    (void)data;
+    if (mp->battery > 1000)  // bug fix: any value lower than 1v means the DMA reading is not ready
+        sprintf(mp->tmpstr, "%2d.%02dV", mp->battery / 1000, (mp->battery % 1000) / 10);
+    else
+        mp->tmpstr[0] = 0;
+    return mp->tmpstr;
+}
+
+s16 trim_cb(void * data)
+{
+    s8 *trim = (s8 *)data;
+    return *trim;
+}
+
+s16 bar_cb(void * data)
+{
+    u8 idx = (long)data;
+    return MIXER_GetChannel(idx-1, APPLY_SAFETY);
+}
+
+void PAGE_MainEvent()
+{
+    int i;
+    int clear_time = 0;
+    if (PAGE_GetModal()) {
+        if(pagemem.modal_page == 2) {
+            PAGE_TelemtestEvent();
+        }
+        return;
+    }
+    for(i = 0; i < 6; i++) {
+        if (! OBJ_IS_USED(&gui->trim[i]))
+            continue;
+        if (mp->trims[i] != Model.trims[i].value) {
+            mp->trims[i] = Model.trims[i].value;
+            GUI_Redraw(&gui->trim[i]);
+        }
+    }
+    for(i = 0; i < 8; i++) {
+        if (! OBJ_IS_USED(&gui->box[i]))
+            continue;
+        s32 val = get_boxval(Model.pagecfg.box[i]);
+        if (Model.pagecfg.box[i] <= NUM_TIMERS) {
+            if ((val >= 0 && mp->boxval[i] < 0) || (val < 0 && mp->boxval[i] >= 0)) {
+                //Timer
+                GUI_SetLabelDesc(&gui->box[i], get_box_font(i, val < 0));
+                mp->boxval[i] = val;
+                GUI_Redraw(&gui->box[i]);
+            } else if (mp->boxval[i] / 1000 != val /1000) {
+                mp->boxval[i] = val;
+                GUI_Redraw(&gui->box[i]);
+            }
+        } else if (Model.pagecfg.box[i] - NUM_TIMERS <= NUM_TELEM) {
+            u32 time = CLOCK_getms();
+            if (Telemetry.time[0] && time - Telemetry.time[0] > TELEM_ERROR_TIME) {
+                clear_time = 1;
+                GUI_SetLabelDesc(&gui->box[i], get_box_font(i, 1));
+            } else if(Telemetry.time[0] && mp->boxval[i] != val) {
+                GUI_SetLabelDesc(&gui->box[i], get_box_font(i, 0));
+                mp->boxval[i] = val;
+                GUI_Redraw(&gui->box[i]);
+            }
+        } else if (mp->boxval[i] != val) {
+            mp->boxval[i] = val;
+            GUI_Redraw(&gui->box[i]);
+        }
+    }
+    if (clear_time)
+        Telemetry.time[0] = 0;
+    volatile s16 *raw = MIXER_GetInputs();
+    for(i = 0; i < 8; i++) {
+        if (! OBJ_IS_USED(&gui->bar[i]))
+            continue;
+        s16 chan = MIXER_GetChannel(Model.pagecfg.bar[i]-1, APPLY_SAFETY);
+        if (mp->barval[i] != chan) {
+            mp->barval[i] = chan;
+            GUI_Redraw(&gui->bar[i]);
+        }
+    }
+    for(i = 0; i < 4; i++) {
+        if (! OBJ_IS_USED(&gui->toggle[i]))
+            continue;
+        u8 src = MIXER_SRC(Model.pagecfg.toggle[i]);
+        struct ImageMap img;
+        (void)img.x_off;
+        (void)img.y_off;
+        img.file = NULL;
+        if (Model.pagecfg.toggle[i] > INP_HAS_CALIBRATION && Model.pagecfg.toggle[i] < INP_LAST) {
+            //switch
+            for (int j = 0; j < 3; j++) {
+                // Assume switch 0/1/2 are in order
+                if(Model.pagecfg.tglico[i][j] && raw[src+j] > 0) {
+                    img = TGLICO_GetImage(Model.pagecfg.tglico[i][j]);
+                }
+            }
+        } else {
+            //Non switch
+            int sw = raw[src] > 0 ? 1 : 0;
+            if (Model.pagecfg.tglico[i][sw]) {
+                img = TGLICO_GetImage(Model.pagecfg.tglico[i][sw]);
+            }
+        }
+        if (img.file) {
+            GUI_ChangeImage(&gui->toggle[i], img.file, img.x_off, img.y_off);
+            GUI_SetHidden((guiObject_t *)&gui->toggle[i], 0);
+        } else {
+            GUI_SetHidden((guiObject_t *)&gui->toggle[i], 1);
+        }
+    }
+    _check_voltage();
+}
