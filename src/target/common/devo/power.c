2d6ec52134fcb3a96e6119008aab8de88d22712c
PhracturedBlue <rc2012@pblue.org>
2014-05-10 15:12:10 -0700

    Fix Frsky telemetry capture.  Only V1, V2, and RSSI have been identified so far

---
+++ src/target/common/devo/power.c
@@ -0,0 +1,90 @@
+/*
+    This project is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Deviation is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/f1/rcc.h>
+#include <libopencm3/cm3/scb.h>
+#include "common.h"
+#include "devo.h"
+
+void PWR_Init(void)
+{
+    SCB_VTOR = VECTOR_TABLE_LOCATION;
+    SCB_SCR  &= ~SCB_SCR_SLEEPONEXIT; //sleep immediate on WFI
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+
+    /* Enable GPIOA so we can manage the power switch */
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, _PWREN_RCC_APB2ENR_IOPEN);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, _PWRSW_RCC_APB2ENR_IOPEN);
+
+    /* Pin 2 controls power-down */
+    gpio_set_mode(_PWREN_PORT, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_PUSHPULL, _PWREN_PIN);
+
+    /* Enable GPIOA.2 to keep from shutting down */
+    gpio_set(_PWREN_PORT, _PWREN_PIN);
+
+    /* When Pin 3 goes high, the user turned off the Tx */
+    gpio_set_mode(_PWRSW_PORT, GPIO_MODE_INPUT,
+                  GPIO_CNF_INPUT_FLOAT, _PWRSW_PIN);
+
+    /* Disable SWD and set SWD pins as I/O for programable switch */
+    AFIO_MAPR = (AFIO_MAPR & ~AFIO_MAPR_SWJ_MASK) | AFIO_MAPR_SWJ_CFG_JTAG_OFF_SW_OFF;
+    gpio_set_mode(GPIO_BANK_JTMS_SWDIO, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_PUSHPULL, GPIO_JTMS_SWDIO);
+    gpio_set(GPIO_BANK_JTMS_SWDIO, GPIO_JTMS_SWDIO);
+    gpio_set_mode(GPIO_BANK_JTCK_SWCLK, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_PUSHPULL, GPIO_JTCK_SWCLK);
+    gpio_set(GPIO_BANK_JTCK_SWCLK, GPIO_JTCK_SWCLK);
+}
+
+void PWR_Shutdown()
+{
+    printf("Shutdown\n");
+    BACKLIGHT_Brightness(0);
+    rcc_set_sysclk_source(RCC_CFGR_SW_SYSCLKSEL_HSICLK);
+    rcc_wait_for_osc_ready(HSI);
+    gpio_clear(_PWREN_PORT, _PWREN_PIN);
+    while(1) ;
+}
+
+int PWR_CheckPowerSwitch()
+{
+    //static u16 debounce = 0;
+    if(gpio_get(_PWRSW_PORT, _PWRSW_PIN)) {
+/*
+        debounce++;
+    } else {
+        debounce = 0;
+    }
+    if(debounce >= 0x40) {
+*/
+        return 1;
+    }
+    return 0;
+}
+
+void PWR_Sleep()
+{
+    asm("wfi");
+}
+
+/* Return milivolts */
+unsigned PWR_ReadVoltage(void)
+{
+    u32 v = adc_array_raw[NUM_ADC_CHANNELS-1];
+    /* Multily the above by 1000 to get milivolts */
+    v = v * VOLTAGE_NUMERATOR / 100 + VOLTAGE_OFFSET;
+    return v;
+}
---
+++ src/target/common/devo/tx_misc.c
@@ -0,0 +1,57 @@
+#include "common.h"
+#include "config/tx.h"
+
+u8* BOOTLOADER_Read(int idx) {
+    u32 ret = 0x00000000;
+    switch(idx) {
+        case BL_ID: ret = 0x08001000; break;
+    }
+    return (u8*)ret;
+}
+
+void TxName(u8 *var, int len)
+{
+    const u8 * pBLString = (u8*)0x08001000;
+    if(len > 8)
+        len = 8;
+    memcpy(var, pBLString, len - 1);
+    var[len - 1] = 0;
+}
+
+void MCU_SerialNumber(u8 *var, int len)
+{
+    int l = len > 12 ? 12 : len;
+#if defined PROTO_HAS_CYRF6936 && ! defined MODULAR
+    if(Transmitter.txid) {
+        u32 id[4];
+        u32 seed = 0x4d3ab5d0ul;
+        for(int i = 0; i < 4; i++)
+            rand32_r(&seed, Transmitter.txid >> (8*i));
+        for(int i = 0; i < 4; i++)
+            id[i] = rand32_r(&seed, 0x00);
+        memcpy(var, &id[1], len);
+        return;
+    }
+#endif
+    // Every STM32 should have 12 bytes long unique id at 0x1FFFF7E8
+    const u8 *stm32id = (u8*) 0x1FFFF7E8;
+    for(int i = 0; i < l; i++) {
+        var[i] = *stm32id++;
+    }
+    while(l < len) {
+        var[l++] = 0x00;
+    }
+}
+
+void _usleep(u32 x)
+{
+    (void)x;
+    asm ("mov r1, #24;"
+         "mul r0, r0, r1;"
+         "b _delaycmp;"
+         "_delayloop:"
+         "subs r0, r0, #1;"
+         "_delaycmp:;"
+         "cmp r0, #0;"
+         " bne _delayloop;");
+}
