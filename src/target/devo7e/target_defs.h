f02329d9fb2474ba09cc3f549dbf2c674ff88192
PhracturedBlue <rc2012@pblue.org>
2015-06-29 10:08:55 -0700

    Initial commit for devo7e with 256kROM and 2 additional analog pots

--- src/target/devo7e/target_defs.h
+++ src/target/devo7e/target_defs.h
@@ -8,29 +8,27 @@
 #define SPIFLASH_SECTOR_OFFSET 0
 #define SPIFLASH_SECTORS 512
 
-#if defined BUILDTYPE_DEV && ! defined EMULATOR
-//No room for debug and standard gui
- #define HAS_STANDARD_GUI   0
-#else
- #define HAS_STANDARD_GUI   1
-#endif
-
+#define HAS_STANDARD_GUI   1
 #define HAS_ADVANCED_GUI    1
-#define HAS_PERMANENT_TIMER 0
+#define HAS_PERMANENT_TIMER 1
 #define HAS_TELEMETRY       1
 #define HAS_TOUCH           0
 #define HAS_RTC             0
 #define HAS_VIBRATINGMOTOR  1
-#define HAS_DATALOG         0
+#define HAS_DATALOG         1
 #define HAS_SCANNER         0
-#define HAS_LAYOUT_EDITOR   0
+#define HAS_LAYOUT_EDITOR   1
 #define HAS_EXTRA_SWITCHES  OPTIONAL
 #define HAS_MULTIMOD_SUPPORT 1
 #define HAS_VIDEO           0
 
-#define NO_LANGUAGE_SUPPORT 1
+#ifdef BUILDTYPE_DEV
+   #define DEBUG_WINDOW_SIZE 200
+#else
+   #define DEBUG_WINDOW_SIZE 0
+#endif
 
-#define DEBUG_WINDOW_SIZE 0
+
 #define MIN_BRIGHTNESS 0
 #define DEFAULT_BATTERY_ALARM 4100
 #define DEFAULT_BATTERY_CRITICAL 3900
@@ -46,7 +44,7 @@
 #define MAX_POINTS 13
 #define NUM_MIXERS ((NUM_OUT_CHANNELS + NUM_VIRT_CHANNELS) * 4)
 
-#define INP_HAS_CALIBRATION 4
+#define INP_HAS_CALIBRATION 6
 
 /* Compute voltage from y = 2.1592x + 0.2493 */
 #define VOLTAGE_NUMERATOR 216
